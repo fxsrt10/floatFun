933980d20cae359489fc7b9c8d1754b312991cac
picoriley <acloudy@mail.smu.edu>
2014-10-19 01:41:27 -0500

    Items now smoothly return to their default orientations once they're released.

--- floatFun/testProj/Assets/lockRotation.cs
+++ floatFun/testProj/Assets/lockRotation.cs
@@ -3,33 +3,25 @@ using System.Collections;
 using Leap;
 using Leap.Interact;
 
-public class lockRotation : MonoBehaviour {
-
-	private Controller controller;
-	private LeapInteraction leapInteraction;
-	// Use this for initialization
-	void Start () {
-		leapInteraction = this.GetComponent<LeapInteraction> ();
-		controller = new Controller();
-		controller.EnableGesture(Gesture.GestureType.TYPE_SCREEN_TAP);
-		controller.Config.SetFloat("Gesture.ScreenTap.MinForwardVelocity", 30.0f);
-		controller.Config.SetFloat("Gesture.ScreenTap.HistorySeconds", .5f);
-		controller.Config.SetFloat("Gesture.ScreenTap.MinDistance", 1.0f);
-		controller.Config.Save();
-	}
+public class lockRotation : MonoBehaviour
+{
+		private LeapInteraction leapInteraction;
+		private float rotateStart;
+		private bool released = false;
+		// Use this for initialization
+		void Start ()
+		{
+				leapInteraction = this.GetComponent<LeapInteraction> ();
+		}
 	
-	// Update is called once per frame
-	void Update () {
-		//Frame frame = controller.Frame(); // controller is a Controller object
-		//GestureList gesturesInFrame = frame.Gestures();
-		//Gesture gestureOfInterest = gesturesInFrame [0];
-		//Debug.Log (gestureOfInterest.ToString());
-		//if (gestureOfInterest != null)
-			//this.GetComponent<Transform>().rotation = Quaternion.identity;
-		/*HandList hands = frame.Hands;
-		Hand firstHand = hands[0];
-		if(firstHand.GrabStrength == 0.0f)*/
-		if(!leapInteraction.isGrabbed)
-			this.GetComponent<Transform>().rotation = Quaternion.identity;
-	}
+		// Update is called once per frame
+		void Update ()
+		{
+				if (released != leapInteraction.isGrabbed) {
+						rotateStart = Time.time;
+				}
+				if (leapInteraction.isGrabbed == false && this.GetComponent<Transform> ().rotation != Quaternion.identity)
+						this.GetComponent<Transform> ().rotation = Quaternion.Slerp (this.gameObject.transform.rotation, Quaternion.identity, (Time.time - rotateStart) / 5f);
+				released = leapInteraction.isGrabbed;
+		}
 }
