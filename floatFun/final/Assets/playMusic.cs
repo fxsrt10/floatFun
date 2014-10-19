27a97e911695f3ff4ee16be1a831351a84ea3bde
fxsrt10 <fxsrt10@gmail.com>
2014-10-19 10:46:04 -0400

    finalUpdate

---
+++ floatFun/final/Assets/playMusic.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using Leap;
+using Leap.Interact;
+
+public class playMusic : MonoBehaviour {
+
+	private LeapInteraction leapInteraction;
+	public AudioClip ironMan;
+	private bool isPlaying = false;
+	// Use this for initialization
+	void Start () {
+		leapInteraction = this.GetComponent<LeapInteraction> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (leapInteraction.isGrabbed && !isPlaying) {
+
+			AudioSource.PlayClipAtPoint(ironMan, this.GetComponent<Transform>().position);
+			isPlaying = true;
+		}
+	}
+}
